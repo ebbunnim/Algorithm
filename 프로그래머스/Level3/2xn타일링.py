c010508a90557b03b6d57342c50e89e5d9ad906b
ebbunnim <ebbunnim@gmail.com>
2020-08-24 19:18:59 +0900

    add level3

---
+++ 프로그래머스/Level3/2xn타일링.py
@@ -0,0 +1,8 @@
+
+def solution(n):
+    D = [0]*60001
+    D[1] = 1
+    D[2] = 2
+    for i in range(3,n+1):
+        D[i] = (D[i-1]+D[i-2])%1000000007
+    return D[n]
---
+++ 프로그래머스/Level3/네트워크.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# 이어지지 않은 것은, 따로 카운트해야 한다.
+def solution(n, computers):
+    vis = [False] * n
+    ans = 0
+    for i in range(n):
+        if vis[i] == False:
+            bfs(i, vis, n, computers)
+            ans += 1
+
+    return ans + vis.count(False)
+
+
+def bfs(start_node, vis, n, computers):
+    Q = deque()
+    Q.append(start_node)  # 시작되는 노드
+    vis[start_node] = True
+
+    while Q:
+        node = Q.popleft()
+        for i in range(n):  # 열 또는 행이라고 생각하고
+            if vis[i] == False:
+                if computers[i][node] == 1:
+                    vis[i] = True
+                    Q.append(i)
+    return
