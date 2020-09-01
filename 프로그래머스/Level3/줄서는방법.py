d5780c5274044479b9e47c38a33f1d6649ed91ae
ebbunnim <ebbunnim@gmail.com>
2020-09-01 10:07:31 +0900

    ADD

---
+++ 프로그래머스/Level3/줄서는방법.py
@@ -0,0 +1,16 @@
+from math import factorial
+from copy import deepcopy
+
+def solution(n, k):
+    n_list = [i for i in range(1,n+1)]
+    stack = []
+    # k는 슬라이스, 몫은 번수.
+    while n!= 0:
+        cycle = factorial(n-1)
+        curr = (k-1)//cycle #k가 0이면 -1이 됨
+        k%=cycle
+        if k == 0:
+            k = cycle # 이게 중요함.
+        stack.append(n_list.pop(curr))
+        n -= 1
+    return stack
