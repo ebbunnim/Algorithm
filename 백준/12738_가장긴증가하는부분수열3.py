4b35e0e0dd551c47b445ee9edf3692155cfdd1e0
ebbunnim <ebbunnim@gmail.com>
2020-09-10 21:25:55 +0900

    UPDATE

--- 백준/12738_가장긴증가하는부분수열3.py
+++ 백준/12738_가장긴증가하는부분수열3.py
@@ -1,7 +1,3 @@
-import sys
-sys.stdin = open('input.txt','r')
-
-
 def binary(target):
     start = 0
     end = len(D)-1
@@ -15,13 +11,13 @@ def binary(target):
 
 
 if __name__ == '__main__':
+    INF = -1000000001
     N = int(input())
-    N_list = list(map(int, input().split()))
-
-    D = [N_list[0]]
-    for i in range(1,N):
+    N_list =[INF]+list(map(int, input().split()))
+    D = [INF]
+    for i in range(1,N+1):
         if D[-1] < N_list[i]:
             D.append(N_list[i])
         else:
             D[binary(N_list[i])] = N_list[i] # 작은 값으로 갱신. 10-30 이었다면 10-20 으로 해야 더 긴 길이 수열을 찾을 수 있다.
-    print(len(D))
+    print(len(D)-1)
