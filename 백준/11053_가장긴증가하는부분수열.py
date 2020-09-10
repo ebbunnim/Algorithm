d4c15183b5bd4e7d61140c7593404ee69ceb9359
ebbunnim <ebbunnim@gmail.com>
2020-09-10 19:29:22 +0900

    ADD

---
+++ 백준/11053_가장긴증가하는부분수열.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+
+if __name__ == '__main__':
+    N = int(input())
+    N_list = list(map(int, input().split()))
+    D = [0]*N
+    D[0] = 1
+    for i in range(1,N):
+        maxv = 0
+        for j in range(i,-1,-1):
+            if N_list[j] < N_list[i]:
+                if D[j] > maxv:
+                    maxv = D[j]
+        D[i] = maxv+1
+    print(D)
+    print(max(D))
