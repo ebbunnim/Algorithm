507631d5e28ec81e542a45cc8230bf5e98db158c
ebbunnim <ebbunnim@gmail.com>
2020-09-14 23:41:37 +0900

    DP

--- 백준/파이프옮기기1.py
+++ 백준/파이프옮기기1.py
@@ -1,56 +1,28 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-from collections import deque
-
 if __name__ == '__main__':
     # 방향 : 동쪽, 남쪽, 대각선(동쪽방향)
     N = int(input())
     arr = [list(map(int,input().split())) for _ in range(N)]
-    dx = [0, 1, 1]
-    dy = [1, 0, 1]
-    Q = deque()
-    ans = 0
+    D = [[[0]*3 for _ in range(N)] for _ in range(N)]
+    # 가로 0 세로1 대각선 2
+    #(1) D[r][c][0] = D[r][c-1][0] + D[r][c-1][2] # 현재 행, 현재 열, 이전 방
+    #(2) D[r][c][1] = D[r-1][c][1] + D[r-1][c][2]
+    #(3) D[r][c][2] = D[r-1][c-1][0] + D[r-1][c-1][1]+ D[r-1][c-1][2]
+    # 전처리
+    D[0][1][0] = 1
+    for i in range(2,N):
+        if arr[0][i] == 0:
+            D[0][i][0] = D[0][i-1][0]
 
-    def move_by_dir(x, y, dir):
-        if dir == 0 :
-            nx = x + dx[0]
-            ny = y + dy[0]
-            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] != 1:
-                Q.append((nx, ny, 0))
-            nx = x + dx[2]
-            ny = y + dy[2]
-            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] != 1 and arr[x+1][y]!=1 and arr[x][y+1]!=1:
-                Q.append((nx, ny, 2))
-        elif dir == 1:
-            nx = x + dx[1]
-            ny = y + dy[1]
-            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] != 1:
-                Q.append((nx, ny, 1))
-            nx = x + dx[2]
-            ny = y + dy[2]
-            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] != 1 and arr[x+1][y]!=1 and arr[x][y+1]!=1:
-                Q.append((nx, ny, 2))
-        else:# dir = 2
-            if 0 <= x < N and 0 <= y+1 < N and arr[x][y+1] != 1:
-                Q.append((x, y+1, 0))
-            if 0 <= x+1 < N and 0 <= y < N and arr[x+1][y] != 1:
-                Q.append((x+1, y, 1))
-            if 0 <= x+1 < N and 0 <= y+1 < N and arr[x][y+1] != 1 and arr[x+1][y] != 1 and arr[x+1][y+1] != 1:
-                Q.append((x+1,y+1, 2))
+    # 벽이 있다는 제약에 주
+    for r in range(1,N):
+        for c in range(1,N):
+            if arr[r][c] == 0: # 현재에 대한 확인
+                D[r][c][0] = D[r][c-1][0] + D[r][c-1][2]
+                D[r][c][1] = D[r-1][c][1] + D[r-1][c][2]
+            if arr[r][c] == 0 and arr[r-1][c] == 0 and arr[r][c-1]==0: # 현재와 바로 전이 현재에 미친 영향을 고려
+                D[r][c][2] = D[r - 1][c - 1][0] + D[r - 1][c - 1][1] + D[r - 1][c - 1][2]
 
-
-    def bfs(x, y, dir):
-        global ans
-        Q.append((x,y,dir))
-        while Q :
-            x, y, dir = Q.popleft()
-            if (x,y) == (N-1,N-1):
-                ans += 1
-                continue
-            move_by_dir(x, y, dir)
-        return
-    bfs(0,1,0) # 앞쪽칸 기준
-    print(ans)
-
-
+    print(D[N-1][N-1][0]+D[N-1][N-1][1]+D[N-1][N-1][2])
