53a10cf826f38d66052495c0e6c01ed9f6c9f474
ebbunnim <ebbunnim@gmail.com>
2020-09-23 13:32:39 +0900

    eratos + two pointer

--- 백준/1644_소수의연속합.py
+++ 백준/1644_소수의연속합.py
@@ -3,18 +3,13 @@ sys.stdin = open('input.txt','r')
 
 if __name__ == '__main__':
     N = int(input())
-    # 41 이하의 소수들 리스트 뽑아.
-    # 소수인지 확인해서 카운트에 1 더해
-    # 나머지는 투 포인터 알고로 풀기
-
-    def eratos():
-        eratos_D = [True]*(1+N)
-        cycle = int(N**0.5)
-        for i in range(2,cycle): # 시작점
-            for j in range(2*i,N+1,i): # 시작점의 배수들
+    eratos_D = [True]*(1+N)
+    cycle = int(N**0.5)
+    for i in range(2,cycle): # 시작점
+        if eratos_D[i] == True: # 시간절약. 소수는 아닌 애의 배수 볼 필요가 없다.
+            for j in range(2*i,N+1,i): # 시작점 배수들
                 eratos_D[j] = False
-        return [i for i in range(2,N+1) if eratos_D[i]==True]
-    series = eratos()
+    series =  [i for i in range(2,N+1) if eratos_D[i]==True]
 
     # 투포인터
     s, e = 0, 0
