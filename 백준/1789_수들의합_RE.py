6ab591a7a5e3ba7b391d8a84fdb505c486bfdb32
ebbunnim <ebbunnim@gmail.com>
2020-10-01 22:54:46 +0900

    BS + versus leetcode33

--- 백준/1789_수들의합_RE.py
+++ 백준/1789_수들의합_RE.py
@@ -1,5 +1,24 @@
 import sys
 sys.stdin = open('input.txt','r')
 
+
+# leetcode 33번과 뭐가 달라?
+# 33번은 값이 있고, 임의적으로 나눈 두 검색 범위 중 어디에 들어가냐의 문
+
+# 여기선, target가 S와 일치하는 순간 없을 수 있음.
+# 특히, s,e가 +1로 좁혀지면 mid가 계속 똑같고 무한루프가 돈다.
+
+
 if __name__ == '__main__':
     S = int(input())
+    s,e = 0, S
+    ans = 0
+    while s<=e: # 어디있는지 모름. 다만, =일순 없다.
+        cnt = (s+e)//2 # 개
+        target = ((cnt+1)*cnt)//2
+        if target <= S:
+            s = cnt+1
+            ans = cnt
+        else:
+            e = cnt-1
+    print(ans)
