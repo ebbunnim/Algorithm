3b1361ab53b74187bf5e3ca0d98ec98c876f33c0
ebbunnim <ebbunnim@gmail.com>
2020-10-06 19:58:07 +0900

    BS / TP

---
+++ 백준/2467_용액.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('/Users/sinjiyoung/PycharmProjects/algorithms_git/algorithm/백준/input.txt','r')
+
+if __name__ == '__main__':
+    N = int(input())
+    Nlist = list(map(int,input().split()))
+    Nlist.sort()
+
+    # s, e : s -> e <- 값이 작아지면 계속 갱신, ans에도
+    # e만 쭉오면서 갱신하다가.
+    # e만 쭉오면서 갱신하다가. 더한 값(0과의 절대값)이 0 보다 작아지는 순간이 생기면,  s를 움직인다.
+    s, e = 0, N-1
+    diff = 2000000000
+    while s<e:
+        sumv = Nlist[s]+Nlist[e]
+        if abs(sumv) < diff:
+            diff = abs(sumv)
+            ans = (s,e)
+        if sumv > 0:
+            e -= 1
+        else:
+            s += 1
+
+        if s==N or e==N:
+            break
+    print(Nlist[ans[0]],Nlist[ans[1]])
+
+
+
---
+++ 백준/2776_암기왕.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('/Users/sinjiyoung/PycharmProjects/algorithms_git/algorithm/백준/input.txt','r')
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        Nlist = list(map(int, input().split()))
+        M = int(input())
+        Mlist = list(map(int, input().split()))
+
+        Nlist.sort()
+        def binary_search(target):
+            s, e = 0, N-1
+            while s<=e:
+                mid = (s+e)//2
+                if Nlist[mid]>target:
+                    e = mid -1
+                elif Nlist[mid]==target:
+                    return 1
+                else:
+                    s = mid + 1
+            return 0
+
+        for target in Mlist:
+            print(binary_search(target))
