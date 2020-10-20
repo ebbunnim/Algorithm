d59f0dc6bdd86ff8010701eee4b124d04dbd7585
ebbunnim <ebbunnim@gmail.com>
2020-10-20 21:31:29 +0900

    [UPDATE] BS

--- 프로그래머스/Level4/**징검다리.py
+++ 프로그래머스/Level4/**징검다리.py
@@ -9,18 +9,16 @@ def solution(distance, rocks, n):
             mid = (s + e) // 2  # 값 자체를 타겟
             cnt = 0
             start = 0
-            minv = distance  # while 문 내부에 있어야 함
             for rock in rocks:
                 if rock - start < mid:
                     cnt += 1  # 제거
                 else:
-                    minv = min(minv, rock - start)
                     start = rock  # 제거되지 않은 바위로 start 바꿔줌
             if cnt > n:
                 e = mid
             else:
                 s = mid + 1
-                ans = minv
+                ans = mid
         return ans
 
     return binary_search()
