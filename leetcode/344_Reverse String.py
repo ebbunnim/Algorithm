78effe451ab6bb08aa8c7b751784de6c7ada8579
ebbunnim <ebbunnim@gmail.com>
2020-12-03 01:11:31 +0900

    two pointers

--- leetcode/344_Reverse String.py
+++ leetcode/344_Reverse String.py
@@ -5,9 +5,8 @@ class Solution:
         """
         l, r = 0, len(s)-1
         while l<r:
-            tmp = s[l]
-            s[l] = s[r]
-            s[r] = tmp 
+            s[l], s[r] = s[r], s[l]
             l+=1
             r-=1
         
+            
