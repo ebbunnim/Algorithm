240863bb21e821ff23395f636b9b6013d7b70e61
ebbunnim <ebbunnim@gmail.com>
2020-12-03 12:11:43 +0900

    정규식+counter

--- leetcode/819_Most Common Word.py
+++ leetcode/819_Most Common Word.py
@@ -1,14 +1,18 @@
 from collections import Counter
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        # split+hash
-        tmp = ''
-        for p in paragraph:
-            if p.isalpha():
-                tmp+=p
-            else:
-                tmp+=' ' # . / , / ' ' 모두 ''로 처리
-        tokens = [x.lower() for x in tmp.split() if x.lower() not in banned ]
-        counter = Counter(tokens)
-        return counter.most_common()[0][0]
+        words = [x for x in re.sub('[^\w]',' ',paragraph).lower().split() if x not in banned]
+        return Counter(words).most_common()[0][0]
+    
+
+        # 이전풀이
+        # tmp = ''
+        # for p in paragraph:
+        #     if p.isalpha():
+        #         tmp+=p
+        #     else:
+        #         tmp+=' ' # . / , / ' ' 모두 ''로 처리
+        # tokens = [x.lower() for x in tmp.split() if x.lower() not in banned ]
+        # counter = Counter(tokens)
+        # return counter.most_common()[0][0]
         
