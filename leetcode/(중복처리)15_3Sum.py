90b9e534154b620d82494e5a9a4eaec04c4c639c
ebbunnim <ebbunnim@gmail.com>
2020-12-04 23:02:43 +0900

    3pointers

---
+++ leetcode/(중복처리)15_3Sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans=[]
+        for i in range(n-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            
+            l, r = i+1, n-1
+            while 0<=l<n and 0<=r<n and l<r:
+                sumv=nums[i]+nums[l]+nums[r]
+                if sumv<0:
+                    l+=1
+                elif sumv>0:
+                    r-=1
+                else:
+                    ans+=[[nums[i],nums[l],nums[r]]]
+                    l+=1
+                    r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
+        return ans
+
