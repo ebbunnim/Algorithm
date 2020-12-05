490971e70dfa9dae06e1a369f746d81d8894e26a
ebbunnim <ebbunnim@gmail.com>
2020-12-05 17:06:22 +0900

    linked list

--- leetcode/(retry할것)206_Reverse Linked List.py
+++ leetcode/(retry할것)206_Reverse Linked List.py
@@ -8,7 +8,7 @@ class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
         while head:
-            # head.next 안건들일 수 있도록 다중할당 사용.단방향 순회 그대로. 근데 포인터 방향은 돌려놔야지
+            # 현재의 head.next 안건들일 수 있도록 다중할당 사용. 단방향 순회 그대로이지만 포인터 방향은 반대로 돌려놓은 상태
             nex, head.next = head.next, prev 
             prev, head = head, nex # prev set, traverse
         return prev
