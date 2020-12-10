87b8c70926eaef998344a966b1c7dfb266e01acd
ebbunnim <ebbunnim@gmail.com>
2020-12-10 17:38:11 +0900

    counter

--- SWEA/D2/1204_최빈수구하기.py
+++ SWEA/D2/1204_최빈수구하기.py
@@ -1,11 +1,13 @@
 import sys
 sys.stdin = open('input.txt','r')
 
+from collections import Counter
 if __name__=="__main__":
     for t in range(int(input())):
         N = int(input())
         Nlist = list(map(int,input().split()))
-
+        counter = Counter(Nlist)
+        print(f'#{t+1} {counter.most_common(1)[0][0]}')
 
 
 
