d9a81338e98294a06527f94a951448eef20e447a
ebbunnim <ebbunnim@gmail.com>
2020-12-12 23:22:14 +0900

    트리순회

--- leetcode/226_Invert Binary Tree.py
+++ leetcode/226_Invert Binary Tree.py
@@ -11,3 +11,35 @@ class Solution:
             return
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
+
+# solution2
+# from collections import deque
+#
+# class Solution:
+#     def invertTree(self, root: TreeNode) -> TreeNode:
+#         Q = deque([root])
+#
+#         while Q:
+#             node = Q.popleft()
+#
+#             if node:
+#                 node.left, node.right = node.right, node.left
+#                 Q += [node.left]
+#                 Q += [node.right]
+#
+#         return root
+
+# solution3 - stack with 전위순회
+# class Solution:
+#     def invertTree(self, root: TreeNode) -> TreeNode:
+#         stack = [root]
+#
+#         while stack:
+#             node = stack.pop()
+#
+#             if node:
+#                 node.left, node.right = node.right, node.left
+#                 stack += [node.left]
+#                 stack += [node.right]
+#
+#         return root
