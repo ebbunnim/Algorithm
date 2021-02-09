1538f70c985ebc8b38f3c71a94a9977414961fef
ebbunnim <ebbunnim@gmail.com>
2021-02-09 16:11:28 +0900

    dfs 사이클 판별

---
+++ 백준/2668_숫자고르기.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+def dfs(curr,stack):
+    vis[curr]=True
+    stack.append(curr)
+    nxt=Nlist[curr]
+    if not vis[nxt]:
+        if not dfs(nxt,stack):
+            return
+    elif vis[nxt] and (nxt in stack):
+        while stack:
+            x=stack.pop()   # 있는 것들 cycle 다 하는 것
+            cycle[x]=True
+            if x==nxt:
+                break
+
+if __name__ == '__main__':
+    N = int(input())
+    Nlist=[0]+[int(input()) for _ in range(N)]
+    cycle=[False]*(1+N)
+    vis=[False]*(1+N)
+    for i in range(1,1+N):
+        dfs(i,[])
+    print(sum(cycle))
+    for i in range(1,N+1):
+        if cycle[i]:
+            print(i)
