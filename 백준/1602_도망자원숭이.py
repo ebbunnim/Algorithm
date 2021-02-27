8b2c317fda65236d33361c8abbb189a94b0f29c8
ebbunnim <ebbunnim@gmail.com>
2021-02-27 22:19:55 +0900

    TLE2

--- 백준/1602_도망자원숭이.py
+++ 백준/1602_도망자원숭이.py
@@ -4,38 +4,38 @@ input = sys.stdin.readline
 
 if __name__ == '__main__':
     N, M, Q = map(int, input().split())
-    penalty = list(map(int, input().split()))
-    key = [(val, idx) for idx, val in enumerate(penalty)]
+    p = list(map(int, input().split()))
+    key = [(val, idx) for idx, val in enumerate(p)]
     key.sort()
     INF = sys.maxsize
     dist = [[INF]*N for _ in range(N)]
-    dist_penalty = [[INF]*N for _ in range(N)]
+
+    penalty=[[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            penalty[i][j]=max(p[i],p[j])
 
     for i in range(N):
-        dist[i][i] = 0
+        dist[i][i] = penalty[i][i]
 
     for _ in range(M):
         a, b, d = map(int, input().split())
         a,b,d=a-1,b-1,d
-        dist[a][b] = d
-        dist[b][a] = d
+        dist[a][b] = d+penalty[a][b]
+        dist[b][a] = d+penalty[b][a]
 
-    for k in key[1:]:  # 차례대로의 정점 번호가 아니라, penalty상으로 오름차순으로 가면 됨.
+    for k in key:  # 차례대로의 정점 번호가 아니라, penalty상으로 오름차순으로 가면 됨.
         val = k[0]
         idx = k[1]
         for i in range(N):
             for j in range(N):
                 if i==j:
                     continue
-                if dist[i][j] > dist[i][idx] + dist[idx][j]:
-                    dist[i][j] = dist[i][idx] + dist[idx][j]
-                max_penalty = max(penalty[i], penalty[j], val)
-                if dist_penalty[i][j] > dist[i][idx] + dist[idx][j] + max_penalty:
-                    dist_penalty[i][j] = dist[i][idx] + dist[idx][j] + max_penalty
-
+                if dist[i][j]>dist[i][idx]-penalty[i][idx]+dist[idx][j]-penalty[idx][j]+max(val,penalty[i][idx],penalty[idx][j]):
+                    dist[i][j]=dist[i][idx] - penalty[i][idx] + dist[idx][j] - penalty[idx][j] + max(val,penalty[i][idx],penalty[idx][j])
     for _ in range(Q):
         S, T = map(int, input().split())
-        if dist_penalty[S-1][T-1] != INF:
-            print(dist_penalty[S-1][T-1])
+        if dist[S-1][T-1] != INF:
+            print(dist[S-1][T-1])
         else:
             print(-1)
