ec0851d2f891596184c7a9a3b3342c5446f3b2e4
ebbunnim <ebbunnim@gmail.com>
2021-03-10 20:19:27 +0900

    dp

---
+++ 백준/11058_크리보드.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+if __name__ == '__main__':
+    N = int(input())
+    DP=[i for i in range(1+N)]
+    if N<=5:
+        print(N)
+    else:
+        for i in range(6,N+1):
+            DP[i]=max(DP[i-1]+1,DP[i-3]*2,DP[i-4]*3)
+        print(DP[N])
