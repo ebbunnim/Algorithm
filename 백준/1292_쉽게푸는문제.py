2c7237af8d37f2105188824fb2077b8f0823a35e
ebbunnim <ebbunnim@gmail.com>
2021-03-16 12:58:31 +0900

    prefix sum

--- 백준/1292_쉽게푸는문제.py
+++ 백준/1292_쉽게푸는문제.py
@@ -4,17 +4,11 @@ input=sys.stdin.readline
 
 if __name__ == '__main__':
     A,B=map(int,input().split())
-    psum=[0]*(B+1)
-    Nlist=[0]*(B+1)
-    flag=0
+    Nlist=[0]
     for i in range(1,B+1):
-        for j in range(1,i+1):
-            if i<=B:
-                Nlist[j]=i
-            else:
-                flag=1
-                break
-        if flag:
-            break
-    print(Nlist)
+        Nlist+=[i]*i
+    psum=[e for e in Nlist]
+    for i in range(1,B+1):
+        psum[i]+=psum[i-1]
+    print(psum[B]-psum[A-1])
 
