906157e51f294ee0d579ec3fca2f82b2b61885f1
ebbunnim <ebbunnim@gmail.com>
2021-03-30 14:33:55 +0900

    76.7% success

--- 프로그래머스/Level3/(카카오)카드짝맞추기_실패.py
+++ 프로그래머스/Level3/(카카오)카드짝맞추기_실패.py
@@ -1,9 +1,8 @@
-from itertools import permutations
 from collections import defaultdict, deque
-from copy import deepcopy
+import sys
 
+minv = sys.maxsize
 def solution(board, r, c):
-    minv = 17
     dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
     kinds = set()
     friends = defaultdict(list)
@@ -13,25 +12,11 @@ def solution(board, r, c):
                 kinds.add(board[i][j])
                 friends[board[i][j]] += [(i, j)]
     n = len(kinds)
-    permu = list(permutations(kinds, n))
-    comb_vis=[False]*n
-    res=[]
-
-    def comb(s,n,cnt):
-        res.append(tuple(comb_vis))
-        if cnt==n:
-            return
-        for i in range(s,n):
-            if comb_vis[i]==False:
-                comb_vis[i]=True
-                comb(i,n,cnt+1)
-                comb_vis[i]=False
-        return
 
     def can_go(r, c, vis):
         return 0 <= r < 4 and 0 <= c < 4 and not vis[r][c]
 
-    def move(r, c, d):
+    def move(r, c, d, copy_board):
         while True:
             nr, nc = r + dr[d], c + dc[d]
             if 0 <= nr < 4 and 0 <= nc < 4:
@@ -42,14 +27,15 @@ def solution(board, r, c):
             else:
                 return r, c
 
-    def bfs(r, c, tr, tc):  # 현재 위치, 타겟 위치
+    def bfs(r, c, tr, tc, copy_board):  # 현재 위치, 타겟 위치
         Q = deque([(r, c, 0)])  # lv -> move에서는 lv의 의미가 확장됨
-        vis = [[False] * 4 for _ in range(4)] # vis가 현재 위치야? - 중복으로 갈 수 있지
-        vis[r][c]=True
+        vis = [[False] * 4 for _ in range(4)]  # vis가 현재 위치야? - 중복으로 갈 수 있지
+        vis[r][c] = True
         while Q:
             r, c, lv = Q.popleft()
             if r == tr and c == tc:
-                return r, c, lv  # 이동 후 위치, 이동한 거리
+                copy_board[r][c] = 0  # 항상 현재 위치는 0으로 만들어야
+                return lv  # 이동 후 위치, 이동한 거리
             # +1씩 사방향
             for d in range(4):
                 nr, nc = r + dr[d], c + dc[d]
@@ -58,35 +44,53 @@ def solution(board, r, c):
                     Q.append((nr, nc, lv + 1))
             # ctrl로 사방향
             for d in range(4):
-                nr, nc = move(r, c, d)
+                nr, nc = move(r, c, d, copy_board)
                 if can_go(nr, nc, vis):
                     vis[nr][nc] = True
                     Q.append((nr, nc, lv + 1))
 
-    comb(0,n,0)
+    def calc(cr,cc,r1,c1,r2,c2,copy_board):
+        res=bfs(cr, cc, r1, c1, copy_board)+bfs(r1, c1, r2, c2, copy_board)+2
+        return res
 
-    for p in permu:
-        for i in range(len(res)): # friends[p[i]][res[i]]
-            copy_board = deepcopy(board)
-            copy_r, copy_c = r, c
-            cnt = 0
-            for j in range(n):
-                # exec
-                friend,flag=p[j],res[i][j]
-                tr, tc = friends[friend][flag][0], friends[friend][flag][1]
-                copy_r, copy_c, lv = bfs(copy_r, copy_c, tr, tc)  # curr -> target
-                cnt += lv
-                copy_board[tr][tc] = 0
-                # same exec
-                tr, tc = friends[friend][not flag][0], friends[friend][not flag][1]
-                copy_r, copy_c, lv = bfs(copy_r, copy_c, tr, tc)  # target -> counter_target
-                cnt += (lv + 2)
-                copy_board[tr][tc] = 0
-                if cnt>minv:
-                    break
-            minv = min(minv, cnt)
-    # ans을 어떻게 카운트할지만 생각하면 됨!
+    vis = [False] * n
+    def backtracking(cnt, stack, flags):
+        global minv
+        if cnt == n:  #
+            res = 0
+            copy_board = [b[:] for b in board]
+            copy_r, copy_c = r, c  # 현재 커서
+            for friend in stack:
+                r1, c1 = friends[friend][flags[friend - 1]]
+                r2, c2 = friends[friend][not flags[friend - 1]]
+                res+=calc(copy_r,copy_c,r1,c1,r2,c2,copy_board)
+                copy_r,copy_c=r2,c2
+                # pruning
+                if res >= minv:
+                    return
+            minv = min(minv, res)
+            return
+
+        for i in range(n):
+            if not vis[i]:
+                vis[i] = True
+                stack += [i+1]
+
+                flags[i] = 1
+                backtracking(cnt+1, stack, flags)
+                flags[i] = 0
+                backtracking(cnt+1, stack, flags)
+
+                stack.pop()
+                vis[i] = False
+
+    backtracking(0, [], [-1] * n)
     return minv
+
+
+
+
+
 
 
 # board=[[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
@@ -96,7 +100,3 @@ r,c=0,1
 
 print(solution(board, r, c))
 
-# 입력
-# 출력
-# 전처리
-# 순차적 - 함수화 / 알고리즘
