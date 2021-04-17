277a5fad41b1edcea90deb44846c6a715e4af7ba
ebbunnim <ebbunnim@gmail.com>
2021-04-17 22:52:20 +0900

    del description

--- 백준/12865_평범한배낭.py
+++ 백준/12865_평범한배낭.py
@@ -1,18 +1,24 @@
-N, K = map(int, input().split())
-stuff = [[0, 0]]
-knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+import sys
+sys.stdin = open('input.txt','r')
+input=sys.stdin.readline
 
-for _ in range(N):
-    stuff.append(list(map(int, input().split())))
+if __name__ == '__main__':
+    N,K=map(int,input().split())
+    bags=[list(map(int,input().split())) for _ in range(N)]
 
-for i in range(1, N + 1):
-    for j in range(1, K + 1):
-        weight = stuff[i][0]
-        value = stuff[i][1]
+    DP=[[0]*(K+1) for _ in range(N+1)] # 열-무게, 행-N번, 값-value
+    for i in range(1,N+1):
+        w,v=bags[i-1]
+        for total_w in range(1,K+1):
+            if w<=total_w: # 단위당 이득을 불릴 수 있으면 넣겠다.
+                DP[i][total_w]=max(DP[i-1][total_w], DP[i-1][total_w-w]+v)
+            else:
+                DP[i][total_w]=DP[i-1][total_w]
+    print(DP[N][K])
 
-        if j < weight:
-            knapsack[i][j] = knapsack[i - 1][j]
-        else:
-            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
 
-print(knapsack[N][K])
+
+
+
+
+
