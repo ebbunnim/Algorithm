aba143d0e1924bcfee724dc0a53701510f960146
ebbunnim <ebbunnim@gmail.com>
2021-04-21 18:13:44 +0900

    update code - delete listing leaves

--- 프로그래머스/programmers_monthly_2/모두0으로만들기.py
+++ 프로그래머스/programmers_monthly_2/모두0으로만들기.py
@@ -6,7 +6,6 @@ def solution(a, edges):
     global acc
     if sum(a)!=0:
         return -1
-
     def dfs(src,cnt):
         global acc
         if cnt==n:
@@ -17,21 +16,14 @@ def solution(a, edges):
                 dfs(dst,cnt+1)
                 acc+=abs(a[dst])
                 a[src]+=a[dst]
-
     # make graph
     n=len(a)
     graph=[[] for _ in range(n)]
-    check=[0]*n
     for e in edges:
         graph[e[0]]+=[e[1]]
         graph[e[1]]+=[e[0]]
-        check[e[0]]+=1
-        check[e[1]]+=1
-    # leaf 판별, root 노드 지정
-    leaves=[i for i in range(n) if check[i]==1]
-    root=leaves[0]
-    # dfs 탐색
+    # dfs 탐색 (root-0번 지정)
     vis=[False]*n
-    vis[root]=True
-    dfs(root,0)
+    vis[0]=True
+    dfs(0,0)
     return acc
