cb7a8de6cddce72b5b6495eb8fe1b809218fb743
ebbunnim <ebbunnim@gmail.com>
2021-05-05 20:51:49 +0900

    uq

---
+++ 백준/10814_나이순정렬.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt','r')
+input=sys.stdin.readline
+import heapq
+if __name__=="__main__":
+    N=int(input())
+    heap=[]
+    for i in range(N):
+        age,name=input().strip().split()
+        heapq.heappush(heap,(int(age),i,name))
+    while heap:
+        age,_,name = heapq.heappop(heap)
+        print(age,name)
+
+
+
