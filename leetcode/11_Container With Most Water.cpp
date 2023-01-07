8995ee8ed81a0edfcc7af676d0b682fca7a754f2
ebbunnim <ebbunnim@gmail.com>
2023-01-08 01:59:34 +0900

    알고리즘: 투포인터

--- leetcode/11_Container With Most Water.cpp
+++ leetcode/11_Container With Most Water.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n= height.size();
-        int ans= INT_MIN;
-        
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                int a= min(height[i], height[j])*(j-i);
-                ans = max(ans,a);
+        int left = 0;
+        int right = height.size()-1;
+        int ans = (right-left) * min(height[left],height[right]);
+        while (left<right) {   
+            if (height[left]<height[right]) {
+                left++;
+            } else {
+                right--;
             }
+            ans = max(ans, (right-left)*min(height[left],height[right]));
+            
         }
         return ans;
     }
