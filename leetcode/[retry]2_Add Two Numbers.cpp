687763a227efbe343cd1d61d9f01cc24a015c907
ebbunnim <ebbunnim@gmail.com>
2023-01-12 23:43:43 +0900

    자료구조: 연결리스트

---
+++ leetcode/[retry]2_Add Two Numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { 
+        int carry = 0;
+        ListNode head; 
+        ListNode* temp = &head;
+        while (l1 || l2) {
+            printf("===========");
+            int val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            carry = val/10; 
+            temp->next = new ListNode(val%10); 
+            temp = temp->next; 
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
+        }
+        if (carry) {
+            printf("-------");
+            temp->next = new ListNode(carry);
+        }
+        return head.next;    }
+};
