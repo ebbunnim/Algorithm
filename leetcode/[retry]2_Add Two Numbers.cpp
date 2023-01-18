c4ede7778a82a9badc3187f62c8d8a146c37eac9
ebbunnim <ebbunnim@gmail.com>
2023-01-18 22:09:55 +0900

    자료구조: 연결리스트

--- leetcode/[retry]2_Add Two Numbers.cpp
+++ leetcode/[retry]2_Add Two Numbers.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { 
+        // Q. 아래 두 줄의 의미. 왜 dummy만 생성하면 안되는지? Q. ListNode dummy; ListNode* currNode = &dummy; 와 같은 의미인지 확인
+        ListNode* dummy = new ListNode(); 
+        ListNode* currNode = dummy; 
+        int addResult;
         int carry = 0;
-        ListNode head; 
-        ListNode* temp = &head;
         while (l1 || l2) {
-            printf("===========");
-            int val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
-            carry = val/10; 
-            temp->next = new ListNode(val%10); 
-            temp = temp->next; 
-            if (l1)
-                l1 = l1->next;
-            if (l2)
-                l2 = l2->next;
+            addResult = carry + (l1? l1->val : 0) + (l2 ? l2->val : 0); // Q. carry까지 더하는 이유?
+            carry = addResult/10; 
+            currNode->next = new ListNode((addResult%10));
+            currNode = currNode->next; 
+            if (l1) 
+                l1 = l1->next; 
+            if (l2) 
+                l2 = l2->next; 
         }
-        if (carry) {
-            printf("-------");
-            temp->next = new ListNode(carry);
-        }
-        return head.next;    }
-};
+        if (carry)
+            currNode->next = new ListNode(carry);
+        return dummy->next; // Q. dummy.next vs dummy->next
+}};
