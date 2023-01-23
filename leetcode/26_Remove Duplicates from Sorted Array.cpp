7918cf59952e8e1f36caf1c465b72776f3a2cb7a
ebbunnim <ebbunnim@gmail.com>
2023-01-23 22:56:59 +0900

    자료구조: array

--- leetcode/26_Remove Duplicates from Sorted Array.cpp
+++ leetcode/26_Remove Duplicates from Sorted Array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int removeDuplicates(vector<int>& nums) {
         int inplaceIdx = 0; 
@@ -11,5 +11,17 @@ public:
             }
         }
         return inplaceIdx;
+    }
+};
+
+class Solution2 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 0; // pointer 1
+        for (int num : nums) { // value by pointer 2
+            if (!idx || num > nums[idx-1])
+                nums[idx++] = num; 
+        }
+        return idx;
     }
 };
