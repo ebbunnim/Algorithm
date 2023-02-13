3a40a503ec1fa3fc1e26f8f68aa9befe8ac9aef4
ebbunnim <ebbunnim@gmail.com>
2023-02-13 22:32:47 +0900

    자료구조:unordered_map

--- leetcode/205_Isomorphic Strings.cpp
+++ leetcode/205_Isomorphic Strings.cpp
@@ -1,18 +1,36 @@
+// Solution 1
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> sUmap;
         unordered_map<char, char> tUmap;
         for (int idx=0; idx<s.size(); idx++) {
-            if (sUmap.find(s[idx]) == sUmap.end() && tUmap.find(t[idx]) == tUmap.end()) { // 둘다 없다.
+            if (sUmap.find(s[idx]) == sUmap.end() && tUmap.find(t[idx]) == tUmap.end()) { // key absent (all umap)
                 sUmap[s[idx]] = t[idx];
                 tUmap[t[idx]] = s[idx];
-            } else if ( sUmap.find(s[idx]) != sUmap.end() && tUmap.find(t[idx]) != tUmap.end() ) { 
+            } else if ( sUmap.find(s[idx]) != sUmap.end() && tUmap.find(t[idx]) != tUmap.end() ) { // key exists (all umap)
                 if (sUmap[s[idx]]!=t[idx] || tUmap[t[idx]]!=s[idx]) 
                     return false;
             } else {
                 return false;
             }
+        }
+        return true;
+
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> sUmap;
+        unordered_map<char, char> tUmap;
+        for (int idx=0; idx<s.size(); idx++) {
+            if (sUmap[s[idx]] && sUmap[s[idx]]!=t[idx]) return false;
+            if (tUmap[t[idx]] && tUmap[t[idx]]!=s[idx]) return false;
+            sUmap[s[idx]]=t[idx];
+            tUmap[t[idx]]=s[idx];
         }
         return true;
 
