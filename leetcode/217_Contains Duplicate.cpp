6fa89d62fadc74c3cb56c37bac5367523cd0a7d7
ebbunnim <ebbunnim@gmail.com>
2023-02-14 21:41:46 +0900

    자료구조:unordered_set

--- leetcode/217_Contains Duplicate.cpp
+++ leetcode/217_Contains Duplicate.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -6,6 +7,19 @@ public:
             if (uMap.find(nums[idx])==uMap.end()) 
                 uMap[nums[idx]] = 1;
             else 
+                return true;
+        }
+        return false;
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> uSet;
+        for (int idx=0;idx<nums.size();idx++) {
+            if (uSet.insert(nums[idx]).second==false) // duplicated
                 return true;
         }
         return false;
