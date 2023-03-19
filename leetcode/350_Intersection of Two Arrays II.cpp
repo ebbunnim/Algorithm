b33f6b552d809043eb282a91e9ef7fb5b5e54798
jaylene <jaylene.shin@kakaocorp.com>
2023-03-19 21:57:56 +0900

    알고리즘:투포인터

---
+++ leetcode/350_Intersection of Two Arrays II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int left=0;
+        int right=0;
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        while (left<nums1.size() && right<nums2.size()) {
+            while (left<nums1.size() && nums1[left]<nums2[right]) 
+                left++;
+            while (right<nums2.size() && nums1[left]>nums2[right]) 
+                right++;
+            while (left<nums1.size() && right<nums2.size() && nums1[left]==nums2[right]) {
+                ans.push_back(nums1[left]);
+                left++;
+                right++;
+            }
+        }
+        return ans;
+    }
+};
