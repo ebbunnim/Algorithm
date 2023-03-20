65333add2dfaf52a5e2b7e8b60251fd2b66a0400
ebbunnim <ebbunnim@gmail.com>
2023-03-20 23:19:03 +0900

    자료구조:스트링

---
+++ leetcode/434_Number of Segments in a String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int ans = 0;
+        int findAlpha = false;
+        for (int idx=0; idx<s.size(); idx++) {
+            if (s[idx]!=' ') {
+                findAlpha = true;
+            } else {
+                if (findAlpha) {
+                    ans++;
+                    findAlpha = false;
+                }
+            }
+        }
+        if (findAlpha) 
+            ans++;
+        return ans;
+    }
+};
